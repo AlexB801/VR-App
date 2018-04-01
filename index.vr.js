fdc4b55dc357b3f8509dab4c7572faf125a04932
Alex Bennett <benedictus.ab@gmail.com>
2018-03-31 22:03:36 -0600

    Changed Styling

--- index.vr.js
+++ index.vr.js
@@ -14,12 +14,8 @@ export default class FirstVRApp extends React.Component {
         <Pano source={asset('skyvr.jpg')}/>
         <Text
           style={{
-            // backgroundColor: '#777879',
             fontSize: 0.3,
-            // fontWeight: '350',
             layoutOrigin: [0.5, 0.5],
-            // paddingLeft: 0.2,
-            // paddingRight: 0.2,
             textAlign: 'center',
             textAlignVertical: 'center',
             transform: [{translate: [0, 0, -3]}],
